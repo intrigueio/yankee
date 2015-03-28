6c03bf03865d60d65f63ecf1c7d0268877371e08
jcran <jcran@pentestify.com>
2015-03-28 16:37:03 -0700

    functional tweaks

--- api.rb
+++ api.rb
@@ -7,7 +7,7 @@ class Yankee < Sinatra::Base
   set :bind, "0.0.0.0"
   set :port, 8888
   set :static, true                             # set up static file routing
-  #set :public, File.expand_path('..', __FILE__) # set up the static dir (with images/js/css inside)
+  set :public, File.expand_path(File.expand_path(".")) # set up the static dir (with images/js/css inside)
 
   #set :views,  File.expand_path('../views', __FILE__) # set up the views dir
   #set :haml, { :format => :html5 }                    # if you use haml
@@ -22,18 +22,20 @@ class Yankee < Sinatra::Base
     sort = params["sort"]
 
     if sort == "Modified"
-      entries = Dir.entries('.').sort_by { |x| File.mtime(x) }
+      entries = Dir.entries('.').sort_by{ |x| File.mtime(x) }.reverse
     else
-      entries = Dir.entries('.').sort_by { |x| File.size(x) }
+      entries = Dir.entries('.').sort_by{ |x| File.size(x) }.reverse
     end
 
+    #entries = entries[0..100]
+
     out = ""
-    out << "<table>"
-    out << "<tr><td>Filename</td><td><a href=/?sort=>Size</a><td><td><a href=/?sort=Modified>Modified</a><td></tr><br/>"
+    out << "<html><body><table>"
+    out << "<tr><td>Filename</td><td><a href=/?sort=>Size</a><td><td><a href=/?sort=Modified>Modified</a><td></tr>"
     entries.map do |e|
-      out << "<tr><td>#{e}</td><td>#{File.size(e)}<td><td>#{File.mtime(e)}<td></tr><br/>"
+      out << "<tr><td><a href=\"#{e}\">#{e}</a></td><td>#{File.size(e)}<td><td>#{File.mtime(e)}<td></tr>"
     end
-    out << "</table>"
-  out.join("\n")
+    out << "</table></body></html>"
+  out
   end
 end
